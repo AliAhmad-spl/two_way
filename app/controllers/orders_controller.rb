26a4306544584755837016ce17e88a41946d3823
muhammad-umar-pisces <umar032190@gmail.com>
2019-12-11 11:00:00 +0500

    byebug removed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -26,7 +26,6 @@ class OrdersController < ApplicationController
   def create
     
     @order = Order.new(order_params)
-    byebug
     respond_to do |format|
       if @order.save
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
