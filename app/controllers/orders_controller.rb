3dc640982fa03e4723bf9b5016f559ff5ddde14e
muhammad-umar-pisces <umar032190@gmail.com>
2019-12-12 01:13:09 +0500

    changes for front page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,7 +28,7 @@ class OrdersController < ApplicationController
     @order = Order.new(order_params)
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to @order }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
