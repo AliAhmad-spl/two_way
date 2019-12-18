eb032dbc8f09e2f27f5a1c5f0e7545f65c7706a2
aliahmad66 <ali.ahmed@buzzinteractive.co>
2019-12-18 17:05:56 +0500

    byebug removed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,7 +24,6 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    byebug
     @order = Order.new(order_params)
     respond_to do |format|
       if @order.save
