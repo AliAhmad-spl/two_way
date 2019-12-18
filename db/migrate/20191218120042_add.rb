df4dd4b5464eb5c43de46f8cfdd20dbb22598e6e
aliahmad66 <ali.ahmed@buzzinteractive.co>
2019-12-18 17:27:48 +0500

    table number issue

--- db/migrate/20191218120042_add.rb
+++
@@ -1,6 +0,0 @@
-class Add < ActiveRecord::Migration[5.2]
-  def change
-  	remove_column :orders, :quntity, :integer
-  	add_column :orders, :quntities, :integer, array: true, default: []
-  end
-end
